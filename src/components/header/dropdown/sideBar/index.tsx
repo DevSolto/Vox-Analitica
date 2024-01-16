149a0c3b2011e066506d59f06f02a3fa410527cc
Mestre Santiago <souto.santiago@academico.ifpb.edu.br>
2024-01-16 15:24:05 -0300

    v 1.2

--- src/components/header/dropdown/sideBar/index.tsx
+++ src/components/header/dropdown/sideBar/index.tsx
@@ -9,9 +9,9 @@ export function SideBar({ active }: SideBarProps) {
   return (
     <>
       <div className="fixed top-0 left-0 w-full bg-primary flex flex-col gap-4 py-4 shadow-lg">
-        <Item active={active} link="#sobre" label="Sobre" />
-        <Item active={active} link="#servicos" label="Serviços" />
-        <Item active={active} link="#contato" label="Contato" />
+        <Item active={active} link="#about" label="Sobre" />
+        <Item active={active} link="#services" label="Serviços" />
+        <Item active={active} link="#contatt" label="Contato" />
       </div>
       <button
         className="text-white fixed top-4 right-4 flex"
