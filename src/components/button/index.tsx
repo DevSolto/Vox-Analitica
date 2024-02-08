5abe2c2ed6c4d75959248ec78eccf7b43102c1c1
Mestre Santiago <souto.santiago@academico.ifpb.edu.br>
2024-02-08 10:05:39 -0300

    Atualiza componente Button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
 
 export function Button({ text }: ButtonProps) {
   return (
-    <button className="bg-highlights text-white font-bold py-2 lg:px-8  lg:max-w-40 w-[100%] rounded-full lg:mt-10 mb-20 hover:bg-white hover:text-highlights transition-all">
+    <button className="bg-highlights text-white font-bold py-2 lg:px-8 px-4 lg:max-w-40 w-[100%] rounded-full lg:mt-10 mb-20 hover:bg-white hover:text-highlights transition-all">
       {text}
     </button>
   );
